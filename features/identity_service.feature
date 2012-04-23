0d70de04719bb4fa820e54fb93db0f2e3bc3c0e6
Etienne Pelletier <epelletier@morphlabs.com>
2012-04-23 09:29:19 -0500

    Added tests for tenants and roles

--- features/identity_service.feature
+++ features/identity_service.feature
@@ -7,3 +7,11 @@ Feature: Authentication
   Scenario: The identity service should return a non-empty list of users when queried.
     When the list of users is retrieved from the identity service
     Then the list of users should not be empty
+
+  Scenario: The identity service should return a non-empty list of tenants when queried.
+    When the list of tenants is retrieved from the identity service
+    Then the list of tenants should not be empty
+
+  Scenario: The identity service should return a non-empty list of roles when queried.
+    When the list of roles is retrieved from the identity service
+    Then the list of roles should not be empty
--- features/step_definitions/identity_service_steps.rb
+++ features/step_definitions/identity_service_steps.rb
@@ -12,3 +12,19 @@ end
 Then /^the list of users should not be empty$/ do
   @list_user_response.body['users'].empty?.should_not == true
 end
+
+When /^the list of tenants is retrieved from the identity service$/ do
+  @list_tenants_response = @identity_service.list_tenants
+end
+
+Then /^the list of tenants should not be empty$/ do
+  @list_tenants_response.body['tenants'].empty?.should_not == true
+end
+
+When /^the list of roles is retrieved from the identity service$/ do
+  @list_roles_response = @identity_service.list_roles
+end
+
+Then /^the list of roles should not be empty$/ do
+  @list_roles_response.body['roles'].empty?.should_not == true
+end
