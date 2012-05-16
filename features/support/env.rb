dc45541565d27013e8074ef61f77542ae53bf04f
Etienne Pelletier <epelletier@morphlabs.com>
2012-05-15 21:37:49 -0500

    Added compute and volume services.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,5 @@
 require 'fog'
 
-
 CONNECTION_PARAMS ||= { :provider => 'OpenStack',  \
                         :openstack_auth_url => "http://#{ENV['FOGHORN_OS_AUTH_HOST']}:#{ENV['FOGHORN_OS_AUTH_PORT'] || 35357}/v2.0/tokens", \
                         :openstack_tenant => ENV['FOGHORN_OS_TENANT'], \
@@ -32,3 +31,19 @@ end
 
 World(KnowsImageService)
 
+
+module KnowsComputeService
+  def compute_service
+    Fog::Compute.new(connection_parameters)
+  end
+end
+
+World(KnowsComputeService)
+
+module KnowsVolumeService
+  def volume_service
+    Fog::Volume.new(connection_parameters)
+  end
+end
+
+World(KnowsVolumeService)
